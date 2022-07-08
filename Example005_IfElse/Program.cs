a55df3e9e7b244344e26b7e50b84e5244a9e003e
Alexey Lonin <Ininale@gmail.com>
2022-07-08 15:49:15 +0500

    Example 005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "белка")
+{
+    Console.WriteLine("Ура, это же Белка!");
+
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
